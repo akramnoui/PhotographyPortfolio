8cde6dc5730da73d18bc1e72d95130e6ead15c9b
Akram Noui <im_noui@esi.dz>
2020-06-27 20:13:40 +0100

    slideshow working

--- src/App.js
+++ src/App.js
@@ -6,20 +6,36 @@ import SlideShow from './Components/SlideShow/SlideShow'
 import MainImage from './Components/MainImage/MainImage' 
 
 
-const alb = [["photo1.jpeg" ],["img5.PNG" ], ["img4.JPG"] , ["img6.jpeg"] , ["img2.jpg"]];
+const alb = [["photo1.jpeg" , "img5.PNG" , "img4.JPG" , "img2.jpg" ],["img5.PNG" ], ["img4.JPG"] , ["img6.jpeg"] , ["img2.jpg"]];
 
 class App extends React.Component {  
   constructor(props){
     super(props);
     this.state={
       Albums: alb , 
-      Album: alb[0] 
+      Album: alb[0],
+      imgindex: 0
     }
     this.handler = this.handler.bind(this);
+    this.nextimg = this.nextimg.bind(this);
+    this.previmg =  this.previmg.bind(this);
   }
+
   handler(index){
     this.setState({
       Album: this.state.Albums[index]
+    })
+  }
+  nextimg(){
+    let index = this.state.imgindex + 1;
+    this.setState({
+      imgindex: index
+    })
+  }
+  previmg(){
+    let index = this.state.imgindex - 1;
+    this.setState({
+      imgindex: index
     })
   }
 
@@ -63,12 +79,12 @@ class App extends React.Component {
        <main>
          <div className="sld-container">
          <SlideShow Albums={this.state.Albums} handler={this.handler} ></SlideShow>
-            <MainImage display={this.state.Album[0]} />
-           <button className="next-slide" >
+            <MainImage display={this.state.Album[this.state.imgindex]} />
+           <button className="next-slide" onClick={this.nextimg} disabled={ this.state.imgindex === this.state.Album.length - 1}  >
              <img className="next-image-1"src={require("./assets/after.png")} ></img>
            </button>
 
-           <button className="prev-slide" >
+           <button className="prev-slide" onClick={this.previmg} disabled={ this.state.imgindex === 0}>
                   <img className="next-image-1"src={require("./assets/before .png")} ></img>
            </button>
 
