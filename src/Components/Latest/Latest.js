5929f96d3c8306043e89a85553a8e7e85ef6e475
Akram Noui <im_noui@esi.dz>
2020-10-10 01:08:48 +0100

    architecture section finished

--- src/Components/Latest/Latest.js
+++ src/Components/Latest/Latest.js
@@ -14,7 +14,7 @@ class Latest extends React.Component{
                 <div className="grid-1">
                    
                     <div className="Latest-work" >
-                        <h1 className="Latest-title">Dive into  Architecture</h1>
+                        <h1 className="Latest-title">Architecture</h1>
                     </div>
                     <div className="Latest-images">
                     <img  src={require("../../assets/arch5.jpeg")}  className="arch5"  />
@@ -26,10 +26,7 @@ class Latest extends React.Component{
                      <img  src={require("../../assets/arch3.jpeg")}  className="arch3"  />
 
                     </div>
-                    <div className="Contact-us" >
-                        <h1 className="Interested">Interested in working with us ?</h1>
-                        <h2 className="let-talk"> Let's talk Now !  </h2>
-                    </div>
+                    
 
                 </div>
                
