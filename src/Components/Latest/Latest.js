b9885573c92524b07743a8da2c1863e989e24e99
Akram Noui <im_noui@esi.dz>
2020-10-11 15:39:32 +0100

    adding featured collection #1

--- src/Components/Latest/Latest.js
+++ src/Components/Latest/Latest.js
@@ -14,12 +14,17 @@ class Latest extends React.Component{
                 <div className="grid-1">
                    
                     <div className="Latest-work" >
-                        <h1 className="Latest-title">| Architecture</h1>
-                        <div className="cover-photo"></div>
+                        <h1 className="Latest-title">| Featured Collection </h1>
+                        
+                        <div className="cover-photo">
+                        <h1 className="image-title"> Marrakech Trip  </h1>
+                        </div>
+
                     </div>
-                    <h1 className="h1-title">
+                    {/* <h1 className="h1-title">
                         Marrakech - Casablanca
-                    </h1>
+                    </h1> */}
+                    <h1 className="capturing-title"> Capturing Architecture</h1>
                     <div className="Latest-images">
                     <img  src={require("../../assets/arch5.jpeg")}  className="arch5"  />
                      <img  src={require("../../assets/arch1.jpg")}  className="arch1"  />
