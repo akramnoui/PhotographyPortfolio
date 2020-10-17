8b512e62abe367d4268ebe8728eed69fc57f64de
Akram Noui <im_noui@esi.dz>
2020-10-17 22:53:16 +0100

    Updates

---
+++ precache-manifest.84abf36bdcf9c40902dde959a37bd42f.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440e2c2b9a0d5af33a18653c8b6e8a19",
+    "url": "/PhotographyPortfolio%20/index.html"
+  },
+  {
+    "revision": "d73469bb2ac509db9c8e",
+    "url": "/PhotographyPortfolio%20/static/css/main.f0721197.chunk.css"
+  },
+  {
+    "revision": "1261516d20b0708e7fd7",
+    "url": "/PhotographyPortfolio%20/static/js/2.5c868b49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PhotographyPortfolio%20/static/js/2.5c868b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73469bb2ac509db9c8e",
+    "url": "/PhotographyPortfolio%20/static/js/main.1f3a0c7d.chunk.js"
+  },
+  {
+    "revision": "7330d0d0a00cf0d804fd",
+    "url": "/PhotographyPortfolio%20/static/js/runtime-main.3908d711.js"
+  },
+  {
+    "revision": "181768ecbb3782afe5507753a4387454",
+    "url": "/PhotographyPortfolio%20/static/media/FullSizeRender-2.181768ec.jpeg"
+  },
+  {
+    "revision": "8e5e8f0bbfdf56a61faf25c40d924df8",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_0165.8e5e8f0b.JPG"
+  },
+  {
+    "revision": "7f3f9573d5db87933e6076f659285141",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_0474.7f3f9573.JPG"
+  },
+  {
+    "revision": "7d716eec61ca926d0ee7243181a11eec",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_0566.7d716eec.JPG"
+  },
+  {
+    "revision": "e927347b5e99cd06ff3a2e35ac3d654b",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_0782.e927347b.jpg"
+  },
+  {
+    "revision": "edda8a6aed767368edbe2f5f43f5a38b",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_1068.edda8a6a.JPG"
+  },
+  {
+    "revision": "260a27d71eb4e1976872e5b058efcc12",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_1826.260a27d7.HEIC"
+  },
+  {
+    "revision": "2afb28fe08f82720d0c14804503e8745",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_6085.2afb28fe.jpeg"
+  },
+  {
+    "revision": "aa7aa15193946d119a31ff94ff0f31bf",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_6456.aa7aa151.jpg"
+  },
+  {
+    "revision": "97064e83fe69f5b8845ef10a2f2da802",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_7448.97064e83.HEIC"
+  },
+  {
+    "revision": "7b6f02c91973a6ee6fa98abf731506f8",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_7790.7b6f02c9.jpg"
+  },
+  {
+    "revision": "46aa384c3cf5272c0a6504ca3694e1de",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_8033.46aa384c.JPG"
+  },
+  {
+    "revision": "cb85d8784dae5353da5982592e958878",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_8051.cb85d878.JPG"
+  },
+  {
+    "revision": "90079c5b8a62dc97b31e91a807c05b54",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_8348.90079c5b.JPG"
+  },
+  {
+    "revision": "0e647b3835bc4e64b05e412e68ac487c",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_8727 (1).0e647b38.jpeg"
+  },
+  {
+    "revision": "64e43f5102ba9b6ee3c199154f14c3c5",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_8727.64e43f51.JPG"
+  },
+  {
+    "revision": "97efd09d5b235a585fa76be161f693e9",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_8851.97efd09d.JPG"
+  },
+  {
+    "revision": "3f46e08b548d6da557a06d360aae190e",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_9605.3f46e08b.JPG"
+  },
+  {
+    "revision": "df4794f1639b8d7dab715b2283c7acd0",
+    "url": "/PhotographyPortfolio%20/static/media/IMG_9928.df4794f1.PNG"
+  },
+  {
+    "revision": "0e9f966589dd702f87b02247ac837bd4",
+    "url": "/PhotographyPortfolio%20/static/media/PA193629-2.0e9f9665.jpeg"
+  },
+  {
+    "revision": "8e5e8f0bbfdf56a61faf25c40d924df8",
+    "url": "/PhotographyPortfolio%20/static/media/ProfileP.8e5e8f0b.JPG"
+  },
+  {
+    "revision": "6431158210c039350416428a643713e1",
+    "url": "/PhotographyPortfolio%20/static/media/arch1.64311582.jpg"
+  },
+  {
+    "revision": "f863d30dda93c481f89bef239a0a9114",
+    "url": "/PhotographyPortfolio%20/static/media/arch2.f863d30d.jpeg"
+  },
+  {
+    "revision": "bcb102b11ad6d5a1801df0d3fdf6f566",
+    "url": "/PhotographyPortfolio%20/static/media/arch3.bcb102b1.jpeg"
+  },
+  {
+    "revision": "00d267cf7c074de944e12b6f7b659337",
+    "url": "/PhotographyPortfolio%20/static/media/arch5.00d267cf.jpeg"
+  },
+  {
+    "revision": "080f9119f0ebbe48f5b7f4017b2110d0",
+    "url": "/PhotographyPortfolio%20/static/media/arch6.080f9119.jpeg"
+  },
+  {
+    "revision": "e927347b5e99cd06ff3a2e35ac3d654b",
+    "url": "/PhotographyPortfolio%20/static/media/backround.e927347b.jpg"
+  },
+  {
+    "revision": "06350ad205ab2af3221d12382216f598",
+    "url": "/PhotographyPortfolio%20/static/media/bg.06350ad2.jpg"
+  },
+  {
+    "revision": "7b6f02c91973a6ee6fa98abf731506f8",
+    "url": "/PhotographyPortfolio%20/static/media/img2.7b6f02c9.jpg"
+  },
+  {
+    "revision": "64e43f5102ba9b6ee3c199154f14c3c5",
+    "url": "/PhotographyPortfolio%20/static/media/img3.64e43f51.JPG"
+  },
+  {
+    "revision": "9e15e3edbe2f55954dd64a8dba088b3a",
+    "url": "/PhotographyPortfolio%20/static/media/img4.9e15e3ed.JPG"
+  },
+  {
+    "revision": "df4794f1639b8d7dab715b2283c7acd0",
+    "url": "/PhotographyPortfolio%20/static/media/img5.df4794f1.PNG"
+  },
+  {
+    "revision": "0e9f966589dd702f87b02247ac837bd4",
+    "url": "/PhotographyPortfolio%20/static/media/img6.0e9f9665.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PhotographyPortfolio%20/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2afb28fe08f82720d0c14804503e8745",
+    "url": "/PhotographyPortfolio%20/static/media/photo1.2afb28fe.jpeg"
+  },
+  {
+    "revision": "212805f9853bf8a3aa6797e0ab9267ac",
+    "url": "/PhotographyPortfolio%20/static/media/photography.212805f9.png"
+  }
+]);
